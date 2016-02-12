17ffc02a88c5b545ee6e15ac2f4d5548b5c8108a
Martin Espinal <marlex.espinal@gmail.com>
2016-02-12 16:53:40 -0600

    Update debug.js

--- src/debug.js
+++ src/debug.js
@@ -1,3 +1,18 @@
-import debug from 'debug';
+'use strict';
 
-export default (name = 'soft-delete') => debug(`loopback:mixins:${name}`);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _debug = require('debug');
+
+var _debug2 = _interopRequireDefault(_debug);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  var name = arguments.length <= 0 || arguments[0] === undefined ? 'soft-delete' : arguments[0];
+  return (0, _debug2.default)('loopback:mixins:' + name);
+};
+
+module.exports = exports['default'];
